cfe0218e8e19b2f166261d7a1e01dfaddaab7b85
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-07 22:12:07 +0800

    feat: define the playPlause func

--- javascript/script.js
+++ javascript/script.js
@@ -3,4 +3,16 @@ const song = document.querySelector("#song");
 const ctrlIcon = document.querySelector("#ctrl-icon");
 const playPauseBtn = document.querySelector("#play-pause");
 
+function playPause() {
+  if (ctrlIcon.classList.contains("fa-pause")) {
+    song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+  } else {
+    song.play();
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+  }
+}
+
 playPauseBtn.addEventListener("click", playPause);
