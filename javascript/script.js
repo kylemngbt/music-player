9992f7d52d5d233cddf23198fbe5e61cf1e0fc8b
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-07 22:07:47 +0800

    build: create a reference of play-pause

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
 const progress = document.querySelector("#progress");
 const song = document.querySelector("#song");
 const ctrlIcon = document.querySelector("#ctrl-icon");
+const playPause = document.querySelector("#play-pause");
+
