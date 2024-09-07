65badbc1c760144b236333e5a76963dde0fca7e2
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-07 22:08:33 +0800

    fix: rename a reference

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 const progress = document.querySelector("#progress");
 const song = document.querySelector("#song");
 const ctrlIcon = document.querySelector("#ctrl-icon");
-const playPause = document.querySelector("#play-pause");
+const playPauseBtn = document.querySelector("#play-pause");
 
